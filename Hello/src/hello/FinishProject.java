5ffa7849439f34b54ddf5f56dec97213f0df423d
Brian313761 <140124219+Brian313761@users.noreply.github.com>
2023-11-03 22:28:13 -0400

    Add files via upload

---
+++ Hello/src/hello/FinishProject.java
@@ -0,0 +1,59 @@
+package hello; 
+
+public class FinishProject{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+/*
+	       String name="Brian";
+	       
+	       char fruit ='a';
+	       
+	       /*
+	         if you put a double quote, computer thinks it's string, not char. but
+	         if you put single quote computer thinks it's character
+	        fruit is a variable(container), and a is the value of fruit variable
+	       */
+	       
+	       
+	       // first we write the 1. datatype 2. variable name 3. value of the variablle 4. put ; to close
+
+          /*  int number1= 5;
+            int number2= 2;
+            
+	       // int ressult	
+	
+          int result= number1 + number2; //variable is like placeholder
+	
+	
+	
+	       System.out.println(result);*/
+	
+		
+		
+		/*Scanner name = new Scanner (System.in);
+		System.out.print("input first name: ");
+		String name1 = name.next();
+		
+		System.out.print("input second name: "); //
+		String name2 = name.next();  
+		
+		System.out.println("Before swapping:  ");
+		
+		System.out.println("name1= " + name1);
+		System.out.println("name2= " + name2);
+		String person = " "; //empty string 
+		
+		person=name2; 
+		name2=name1;
+		name1=person;
+		
+		System.out.println ("After swapping: "); 
+		System.out.println ("name1= " + name1);
+		System.out.println ("name2= " + name2);
+*/
+	}
+}
+
+	
+	
